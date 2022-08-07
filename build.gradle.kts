f1165e632d6ce41581ff6dbc318a9b77397b6fab
Valerii Trebukh <Valerii.Trebukh@gmail.com>
2022-08-07 18:53:59 +0300

    Enabled SSL

--- build.gradle.kts
+++ build.gradle.kts
@@ -35,6 +35,8 @@ dependencies {
     implementation("org.jetbrains.exposed:exposed-jdbc:$exposed_version")
     implementation("com.h2database:h2:$h2_version")
 
+    implementation("io.ktor:ktor-network-tls-certificates:$ktor_version")
+
     implementation("ch.qos.logback:logback-classic:$logback_version")
     testImplementation("io.ktor:ktor-server-tests-jvm:$ktor_version")
     testImplementation("org.jetbrains.kotlin:kotlin-test-junit:$kotlin_version")
